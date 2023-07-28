fcf302b30a888d1118c9ba70b8f52f1cd489b57d
CatherineMeng <myuan2009@gmail.com>
2023-07-28 01:49:03 -0700

    fixed ddlock, changed actor weight sync to within-episode loop

--- MP_runtime/mp_train_learner.py
+++ MP_runtime/mp_train_learner.py
@@ -10,6 +10,7 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import multiprocessing
+import numpy as np
 # from torch.multiprocessing import Process, Pipe
 from multiprocessing import Process, Pipe
 from replay import ReplayMemory
@@ -22,33 +23,29 @@ FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
 
 # Parameters
 n_actors = 4
-capacity = 1000
-batch_size = 64
+capacity = 10000
+batch_size = 128
 gamma = 0.99
 input_size = 4  # CartPole-v0 has an observation space of size 4
 hidden_size = 8
 output_size = 2  # Two possible actions: 0 (left) and 1 (right)
-num_training_eps = 200
+num_training_eps = 1000
 
 # Define a function to run the actor process
 # def actor_process(conn, actor_id, policy_net, memory, n_actions):
 def actor_process(param_conn, actor_id, data_collection_conn):
-    # print("Actor",actor_id,"start")
-    env = gym.make('CartPole-v0')
+    env = gym.make('CartPole-v1')
     policy_net = PolicyNetwork(input_size, hidden_size, output_size)
     print("=== actor", actor_id,"started")
     p_update_cnt=0
-    while True:
-        # rollout_cnt+=1
-        # if (rollout_cnt>1):
-        # print("=== actor", actor_id,"doing epiode")
+    flag_break_outer=0
+    # while True:
+    while not flag_break_outer:
         state = env.reset()[0]
-        # print("  state :", state )
         done = False
         # run one episode 
         rewards=0  
         while not done:
-            
             state_tensor = torch.tensor(state, dtype=torch.float32)
             action_probs = policy_net(state_tensor)
             action = torch.argmax(action_probs).item()
@@ -56,21 +53,28 @@ def actor_process(param_conn, actor_id, data_collection_conn):
             rewards+=reward
             data_collection_conn.send((state, action, reward, next_state, done))
             state = next_state
-        # print("***=== Actor", actor_id,"reward =",rewards)
-        if (param_conn.poll()): # there is updated param to receive
-            try: 
-                parameters = param_conn.recv()  # Receive policy network parameters from the learner
-            except (FileNotFoundError):
-                # FileNotFoundError: [Errno 2] No such file or directory
-                print('Skipped param sync.') 
-            p_update_cnt+=1
-            # print("=== actor", actor_id,"=== received updated params from learner after Learner itr",p_update_cnt)
-            if parameters is not None:
-                policy_net.load_state_dict(parameters)
-        if (p_update_cnt == num_training_eps):
-            print("***=== Actor", actor_id,"DONE TRAINING")
-            data_collection_conn.send("Train done from actor")
-            break
+            if (param_conn.poll()): # there is updated param to receive
+                try: 
+                    parameters = param_conn.recv()  # Receive policy network parameters from the learner
+                except (FileNotFoundError):
+                    # FileNotFoundError: [Errno 2] No such file or directory
+                    print('Skipped param sync.') 
+                except (ConnectionResetError):
+                    # ConnectionResetError: [Errno 104] Connection reset by peer
+                    print('Connection reset by peer.') 
+                except (EOFError):
+                    # raise EOFError
+                    print('Pipe closed, EOFError.') 
+                p_update_cnt+=1
+                # print("=== actor", actor_id,"=== received updated params from learner after Learner itr",p_update_cnt)
+                if parameters is not None:
+                    policy_net.load_state_dict(parameters)
+                    
+            if (p_update_cnt == num_training_eps):
+                print("***=== Actor", actor_id,"DONE TRAINING")
+                data_collection_conn.send("Train done from actor")
+                flag_break_outer=1
+                break
 
     env.close()
 
@@ -79,6 +83,7 @@ def learner_process(param_conns, data_transfer_conn, batch_size, gamma, use_gpu)
     # print("Learner start")
     if use_gpu and torch.cuda.is_available():
         device = torch.device("cuda")
+        print("USING GPU")
     else:
         device = torch.device("cpu")
 
@@ -96,16 +101,14 @@ def learner_process(param_conns, data_transfer_conn, batch_size, gamma, use_gpu)
     # for _ in range(num_training_eps):
         learn_itr_cnt+=1
         if (learn_itr_cnt<=num_training_eps):
-            # print("Learner itr",learn_itr_cnt,"starts")
             policy_net.to(device)
-
             transitions = data_transfer_conn.recv()
             # print("Learner itr",learn_itr_cnt,"received data from master")
             batch = list(zip(*transitions))
-            state_batch = torch.tensor(batch[0], dtype=torch.float32).to(device)
+            state_batch = torch.tensor(np.array(batch[0]), dtype=torch.float32).to(device)
             action_batch = torch.tensor(batch[1], dtype=torch.int64).to(device)
             reward_batch = torch.tensor(batch[2], dtype=torch.float32).to(device)
-            next_state_batch = torch.tensor(batch[3], dtype=torch.float32).to(device)
+            next_state_batch = torch.tensor(np.array(batch[3]), dtype=torch.float32).to(device)
             done_batch = torch.tensor(batch[4], dtype=torch.bool).to(device)
 
             state_action_values = policy_net(state_batch).gather(1, action_batch.unsqueeze(1))
@@ -113,12 +116,12 @@ def learner_process(param_conns, data_transfer_conn, batch_size, gamma, use_gpu)
             expected_state_action_values = (next_state_values * gamma) + reward_batch
 
             loss = F.smooth_l1_loss(state_action_values.squeeze(), expected_state_action_values)
+            # print("loss:",loss)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            # print("Learner itr",learn_itr_cnt,"here")
 
-            if learn_itr_cnt % 10 == 0:  # Update the target network every 10 episodes
+            if learn_itr_cnt % 5 == 0:  # Update the target network every 5 episodes
                 target_net.load_state_dict(policy_net.state_dict())
 
             # Send back the updated policy network parameters to the actors
@@ -129,6 +132,7 @@ def learner_process(param_conns, data_transfer_conn, batch_size, gamma, use_gpu)
         else:
             transitions = data_transfer_conn.recv()
             if (transitions=="Train done from master"):
+                print("Learner: Train Done")
                 break
             for param_conn in param_conns:
                 param_conn.send(policy_net.state_dict())
@@ -166,7 +170,8 @@ def main():
         # for i in range(10000):  # You can adjust the number of training episodes here
         train_cnt=0
         n_actors_done = 0
-        while(1):
+        # while(1):
+        while (n_actors_done==0):
             # replay insertion
             for i,pipe in enumerate(data_collection_pipes):
                 sample = pipe[0].recv()
@@ -186,8 +191,9 @@ def main():
                 train_cnt+=1
                 # print("MASTER: train_cnt", train_cnt, "sent samples to learner")
   
-            if (n_actors_done==1):
-                break #as long as one of the actor is updated with the newest policy, we can exit training
+            # if (n_actors_done==1):
+            #     print("MASTER: Train done")
+            #     break #as long as one of the actor is updated with the newest policy, we can exit training
 
         # Terminate the actor processes and learner process
         for pipe in actor_pipes:
