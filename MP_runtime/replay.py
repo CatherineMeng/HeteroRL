2cf210ea94567914f0d311dcc2ea20e739f63266
CatherineMeng <myuan2009@gmail.com>
2023-07-30 15:46:33 -0700

    replay cpu test success

--- MP_runtime/replay.py
+++ MP_runtime/replay.py
@@ -1,17 +1,270 @@
 import random
+import math
+from collections import namedtuple, deque
+'''
+Uniform distribution
+'''
+class ReplayMemory(object):
 
-class ReplayMemory:
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
+        self.memory = deque([], maxlen=capacity)
 
     def push(self, transition):
+        """Save a transition"""
         self.memory.append(transition)
-        if len(self.memory) > self.capacity:
-            del self.memory[0]
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
 
     def __len__(self):
         return len(self.memory)
+
+import random
+from collections import deque
+
+def Log2(x):
+    return (math.log10(x) / math.log10(2));
+ 
+def n_isPowerOf_f(n,f):
+    if (n == 0):
+        return False
+    while (n != 1):
+        if (n % f != 0):
+            return False
+        n = n // f
+    return True
+
+# binary sum-tree
+class SumTree:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        # assume capacity is a power of 2!
+        assert(math.ceil(Log2(capacity)) == math.floor(Log2(capacity)))
+        # self.tree = [0] * (2 * capacity - 1)
+        self.tree = {key: 0 for key in range(2 * capacity - 1)}
+        self.data_pointer = 0 #pointing to non-leaf and left-most leaf, range[0,capacity), corresponds to data storage dict indices. +capacity-1 to move to corresponding leaf node index
+
+    def add(self, priority):
+        tree_index = self.data_pointer + self.capacity - 1
+        # print("Sum Tree add at data pointer",self.data_pointer,"tree leaf index",self.data_pointer + self.capacity - 1)
+        self.update(tree_index, priority)
+        self.data_pointer = (self.data_pointer + 1) % self.capacity #+1 in leaf, %cap to move back to non-leaf
+
+    def update(self, tree_index, priority): # tree_index is given as the index in the tree, not index in the data storage dict. range: [capacity - 1, 2 * capacity - 1)
+        change = priority - self.tree[tree_index]
+        self.tree[tree_index] = priority
+
+        while tree_index != 0:
+            tree_index = (tree_index - 1) // 2
+            self.tree[tree_index] += change
+
+    # used for sampling, return node indices in the tree (not data storage indices, needs conversion)
+    def get_leaf(self, v):
+        parent_index = 0
+
+        while True:
+            left_child_index = 2 * parent_index + 1
+            right_child_index = left_child_index + 1
+
+            if left_child_index >= len(self.tree): #reached leaf, no more leaf nodes
+                leaf_index = parent_index
+                break
+            else:
+                if v <= self.tree[left_child_index]:
+                    parent_index = left_child_index
+                else:
+                    v -= self.tree[left_child_index] #update target value
+                    parent_index = right_child_index
+
+        return leaf_index, self.tree[leaf_index]
+
+    def total_priority(self):
+        return self.tree[0]
+
+# Prioritized Replay based on a binary sum-tree
+class PrioritizedReplayMemory:
+    def __init__(self, capacity, alpha=0.6, beta=0.4, beta_increment_per_sampling=0.001):
+        self.capacity = capacity
+        self.alpha = alpha
+        self.beta = beta
+        self.beta_increment_per_sampling = beta_increment_per_sampling
+        self.tree = SumTree(capacity)
+        # self.data = deque(maxlen=capacity)
+        self.data = {key: 0 for key in range(capacity)}
+        self.data_pointer = 0
+        self.epsilon = 0.01
+
+    # for inserting a new experience
+    def _get_priority(self, td_error):
+        return (td_error + self.epsilon) ** self.alpha
+
+    def push(self, transition, td_error):
+        priority = self._get_priority(td_error)
+        self.tree.add(priority)
+        # self.data.append(transition)
+        self.data[self.data_pointer] = transition
+        self.data_pointer = (self.data_pointer + 1) % self.capacity
+        # print("pushing data into data storage index",self.data_pointer)
+
+    def sample(self, batch_size):
+        batch = []
+        segment = self.tree.total_priority() / batch_size
+        priorities = []
+        indexes = []
+
+        self.beta = min(1.0, self.beta + self.beta_increment_per_sampling)
+
+        for i in range(batch_size):
+            a, b = segment * i, segment * (i + 1)
+            v = random.uniform(a, b)
+            # get indices from sum tree
+            index, priority = self.tree.get_leaf(v) #index returned are node index in the tree, not index in the data storage dict.
+            indexes.append(index)
+            priorities.append(priority)
+            # convert leaf ndoe index to data storage index, read from data storage
+            batch.append(self.data[index - self.tree.capacity + 1]) 
+            assert(index - self.tree.capacity + 1 >= 0)
+            assert(index - self.tree.capacity + 1 < self.tree.capacity)
+            # print("sampling data with index:",index - self.tree.capacity + 1)
+
+        # sampling_probabilities = priorities / self.tree.total_priority()
+        # is_weights = np.power(self.tree.capacity * sampling_probabilities, -self.beta)
+        # is_weights /= is_weights.max()
+
+        # return batch, indexes, is_weights
+        return batch, indexes
+
+    # index returned by sampling are node index in the tree, not index in the data storage dict.
+    # Therefore, the same tree_indices (as returned by sampling) can be used for update without modification
+    def update_priorities(self, tree_indices, td_errors):
+        for tree_index, td_error in zip(tree_indices, td_errors):
+            assert(tree_index >=- self.tree.capacity - 1)
+            assert(tree_index < 2*self.tree.capacity - 1)
+            priority = self._get_priority(td_error)
+            self.tree.update(tree_index, priority)
+
+
+
+
+# n-ary sum tree
+class SumTreenary:
+    def __init__(self, capacity, fanout):
+        self.capacity = capacity
+        self.fanout = fanout
+        # self.tree = [0] * (2 * capacity - 1)
+        # assume capacity is a power of fanout!
+        assert(n_isPowerOf_f(capacity,fanout))
+        i=0
+        full_tree_size=0
+        while (fanout**i<=capacity):
+            full_tree_size+=fanout**i
+            i+=1
+        print("full tree size:",full_tree_size)
+        self.full_tree_size=full_tree_size
+        # self.full_tree_size - self.capacity = capacity is the total number of non-leaf nodes
+        assert(self.capacity==fanout**(i-1))
+        print("num leaf nodes:",fanout**(i-1))
+    
+        self.tree = {key: 0 for key in range(full_tree_size)}
+
+        self.data_pointer = 0
+
+    def add(self, priority):
+        tree_index = self.data_pointer 
+        self.update(tree_index, priority)
+        # print("insertion updating tree index:",self.data_pointer)
+        self.data_pointer = (self.data_pointer + 1) % self.capacity
+        
+    def update(self, tree_index, priority):
+        
+        change = priority - self.tree[tree_index]
+        self.tree[tree_index] = priority
+
+        while tree_index != 0:
+            tree_index = (tree_index - 1) // self.fanout
+            self.tree[tree_index] += change
+
+    # used for sampling
+    def get_leaf(self, v):
+        parent_index = 0
+
+        while True:
+            left_child_index = self.fanout * parent_index + 1
+            right_child_index = left_child_index + self.fanout-1
+
+            if left_child_index >= len(self.tree):
+                leaf_index = parent_index
+                break
+            else: #TODO: change this to n-ary logic
+                if v <= self.tree[left_child_index]:
+                    parent_index = left_child_index
+                else:
+                    v -= self.tree[left_child_index]
+                    parent_index = right_child_index
+
+        return leaf_index, self.tree[leaf_index]
+
+    def total_priority(self):
+        return self.tree[0]
+
+
+# Prioritized Replay based on a n-ary sum-tree
+class PrioritizedReplayMemory_n:
+    def __init__(self, capacity,fanout, alpha=0.6, beta=0.4, beta_increment_per_sampling=0.001):
+        self.capacity = capacity
+        self.alpha = alpha
+        self.beta = beta
+        self.beta_increment_per_sampling = beta_increment_per_sampling
+        self.tree = SumTreenary(capacity,fanout)
+        # self.data = deque(maxlen=capacity)
+        self.data = {key: 0 for key in range(capacity)}
+        self.data_pointer = 0
+        self.epsilon = 0.01
+
+    # for inserting a new experience
+    def _get_priority(self, td_error):
+        return (td_error + self.epsilon) ** self.alpha
+
+    def push(self, transition, td_error):
+        priority = self._get_priority(td_error)
+        self.tree.add(priority)
+        # self.data.append(transition)
+        self.data[self.data_pointer] = transition
+        # print("pushing data into data storage index",self.data_pointer)
+        self.data_pointer = (self.data_pointer + 1) % self.capacity
+        
+
+    def sample(self, batch_size):
+        batch = []
+        segment = self.tree.total_priority() / batch_size
+        print("self.tree.total_priority():",self.tree.total_priority(),"; segment:",segment)
+        priorities = []
+        indexes = []
+
+        self.beta = min(1.0, self.beta + self.beta_increment_per_sampling)
+
+        for i in range(batch_size):
+            a, b = segment * i, segment * (i + 1)
+            v = random.uniform(a, b)*self.tree.total_priority()
+            # get indices from sum tree
+            index, priority = self.tree.get_leaf(v) #index returned are node index in the tree, not index in the data storage dict.
+            indexes.append(index)
+            priorities.append(priority)
+            # convert leaf ndoe index to data storage index, for reading from data storage
+            batch.append(self.data[index - (self.tree.full_tree_size - self.tree.capacity)]) 
+            # assertions: sampled index is a tree leaf index
+            assert(index >= self.tree.full_tree_size - self.tree.capacity)
+            assert(index < self.tree.full_tree_size)
+            print("sampling data with index:",index - (self.tree.full_tree_size - self.tree.capacity))
+
+        return batch, indexes
+
+    # index returned by sampling are node index in the tree, not index in the data storage dict.
+    # Therefore, the same tree_indices (as returned by sampling) can be used for update without modification
+    def update_priorities(self, tree_indices, td_errors):
+        for tree_index, td_error in zip(tree_indices, td_errors):
+            # assertions: updated index is a tree leaf index
+            assert(tree_index >= self.tree.full_tree_size - self.tree.capacity)
+            assert(tree_index < self.tree.full_tree_size)
+            priority = self._get_priority(td_error)
+            self.tree.update(tree_index, priority)
---
+++ MP_runtime/test_replay.py
@@ -0,0 +1,36 @@
+import random
+from replay import SumTree,PrioritizedReplayMemory
+from replay import SumTreenary,PrioritizedReplayMemory_n
+class TestData:
+    def __init__(self, value):
+        self.value = value
+
+# Test code for ReplayMemory with SumTree
+if __name__ == '__main__':
+
+    # capacity = 27*9
+    capacity = 2**8
+    # fanout=3
+    memory = PrioritizedReplayMemory(capacity)
+    # memory = PrioritizedReplayMemory_n(capacity,fanout)
+
+    # Generate random data for testing
+    random_data = [TestData(random.random()) for _ in range(capacity)]
+
+    # Test add operation
+    for i in range(capacity):
+        priority = random.random()*capacity*100
+        memory.push(random_data[i], priority)
+    memory.push(random_data[i], priority) #test circular buffer. yes
+
+    # Test sample operation
+    batch_size = 64
+    batch, indexes= memory.sample(batch_size)
+    assert len(batch) == batch_size
+    assert len(indexes) == batch_size
+
+    # Test update operation
+    td_errors = [random.random() for _ in range(batch_size)]
+    memory.update_priorities(indexes, td_errors)
+
+    print("All operations (add, sample, update) with SumTree-based PrioritizedReplayMemory passed!")
