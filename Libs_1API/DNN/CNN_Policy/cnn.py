8952bf8120a296da14b156a82eb0ba02e4f57791
CatherineMeng <myuan2009@gmail.com>
2023-08-01 22:13:46 -0700

    add cnn cpu/gpu py

--- Libs_1API/DNN/CNN_Policy/cnn.py
+++ Libs_1API/DNN/CNN_Policy/cnn.py
@@ -6,6 +6,12 @@ import math
 import random
 import time
 
+if torch.cuda.is_available():
+    device = torch.device("cuda")  # Use GPU if available
+    print("USING GPU")
+else:
+    device = torch.device("cpu")
+
 class DQN(nn.Module):
     def __init__(self, input_channels, num_actions):
         super(DQN, self).__init__()
@@ -34,13 +40,16 @@ class DQN(nn.Module):
 class Trainer:
     def __init__(self, input_channels, num_actions, capacity):
         self.network = DQN(input_channels, num_actions)
+        self.network.to(device)
         self.target_network = DQN(input_channels, num_actions)
+        self.target_network.to(device)
         self.dqn_optimizer = optim.Adam(self.network.parameters(), lr=0.0001)
         self.epsilon_start = 1.0
         self.epsilon_final = 0.01
         self.epsilon_decay = 30000
-        self.batch_size = 32
+        self.batch_size = 128
         self.gamma = 0.99
+        self.num_actions=num_actions
         # self.buffer = ExperienceReplay(capacity)  # Uncomment and implement the ExperienceReplay class.
 
     def compute_td_loss(self, batch_size, gamma):
@@ -53,12 +62,12 @@ class Trainer:
         # Instead of loading samples from the buffer, let's create random tensors for demonstration
         batch = []
         for _ in range(batch_size):
-            s_tensor = torch.rand((1, 3, 210, 160), requires_grad=True)
-            ns_tensor = torch.rand((1, 3, 210, 160), requires_grad=True)
-            a_tensor = torch.randint(0, num_actions, (1,),  dtype=torch.long)
-            r_tensor = torch.rand((1,), requires_grad=True)
+            s_tensor = torch.rand((1, 3, 210, 160), requires_grad=True).to(device)
+            ns_tensor = torch.rand((1, 3, 210, 160), requires_grad=True).to(device)
+            a_tensor = torch.randint(0, self.num_actions, (1,), dtype=torch.long).to(device)
+            r_tensor = torch.rand((1,), requires_grad=True).to(device)
             r_tensor = torch.bernoulli(r_tensor)  # random 0 or 1
-            d_tensor = torch.rand((1,), requires_grad=True)
+            d_tensor = torch.rand((1,), requires_grad=True).to(device)
             d_tensor = torch.bernoulli(d_tensor)  # random 0 or 1
             sample = (s_tensor, ns_tensor, a_tensor, r_tensor, d_tensor)
             batch.append(sample)
@@ -95,7 +104,9 @@ class Trainer:
         return epsilon
         
     # The rest of the Trainer class methods are not implemented here for simplicity.
+
     def train(self, random_seed, rom_path, num_epochs, num_actions):
+
         torch.manual_seed(random_seed)
         np.random.seed(random_seed)
         random.seed(random_seed)
@@ -117,7 +128,8 @@ class Trainer:
             if random.random() <= epsilon:
                 action = random.choice(legal_actions)
             else:
-                state_tensor = torch.rand((1, 3, 210, 160))  # Replace with actual environment observation later.
+                state_tensor = torch.rand((1, 3, 210, 160)).to(device)  # Replace with actual environment observation later.
+
                 action_tensor = self.network.act(state_tensor)
                 action = action_tensor.item()
 
@@ -126,11 +138,11 @@ class Trainer:
             episode_reward += reward
             done = bool(random.getrandbits(1))  # Replace with actual environment "done" status later.
 
-            next_state_tensor = torch.rand((1, 3, 210, 160))  # Replace with actual environment observation later.
+            next_state_tensor = torch.rand((1, 3, 210, 160)).to(device)  # Replace with actual environment observation later.
 
-            reward_tensor = torch.tensor(reward)
-            done_tensor = torch.tensor(done, dtype=torch.float32)
-            action_tensor_new = torch.tensor(action, dtype=torch.long)
+            reward_tensor = torch.tensor(reward).to(device)
+            done_tensor = torch.tensor(done, dtype=torch.float32).to(device)
+            action_tensor_new = torch.tensor(action, dtype=torch.long).to(device)
 
             # If using the experience replay buffer, uncomment the following line to push the experience.
             # buffer.push(state_tensor, next_state_tensor, action_tensor_new, done_tensor, reward_tensor)
@@ -138,7 +150,7 @@ class Trainer:
             state_tensor = next_state_tensor
 
             if done:
-                state_tensor = torch.rand((1, 3, 210, 160))  # Renew state if done.
+                state_tensor = torch.rand((1, 3, 210, 160)).to(device)  # Renew state if done.
                 all_rewards.append(episode_reward)
                 episode_reward = 0.0
 
@@ -149,7 +161,6 @@ class Trainer:
 
             if epoch % 1000 == 0:
                 print(episode_reward)
-                # self.loadstatedict(self.network, self.target_network)
                 self.target_network.load_state_dict(self.network.state_dict())
 
         stop = time.time()
