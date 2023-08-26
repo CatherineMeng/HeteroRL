e0761ca6690dac2bf547f14be0c1ea5f661d455d
CatherineMeng <myuan2009@gmail.com>
2023-08-26 16:08:52 -0700

    cleanup

--- CartPole/DevC_Init_notorch/build/src/CMakeFiles/CartPole.fpga.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CartPole.fpga.
-# This may be replaced when dependencies are built.
--- CartPole/DevC_Init_torch/build.sh
+++
@@ -1,6 +0,0 @@
-mkdir build
-cd build
-cmake -DCMAKE_PREFIX_PATH=/home/u186670/libtorch ..
-cmake --build . --config Release
-
-g++ -o notorchout CP_notorch.cpp
