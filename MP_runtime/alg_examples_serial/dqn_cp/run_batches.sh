f0466604b95ccdf12c9ef085672fad3c5c2eb751
CatherineMeng <myuan2009@gmail.com>
2024-03-04 12:40:31 -0800

    rm MP runtime

--- MP_runtime/alg_examples_serial/dqn_cp/run_batches.sh
+++
@@ -1,5 +0,0 @@
-nohup python dqn_mlp_cartpole.py --batch_size=16 > b16log_cpu.txt & disown
-nohup python dqn_mlp_cartpole.py --batch_size=32 > b32log_cpu.txt & disown
-nohup python dqn_mlp_cartpole.py --batch_size=64 > b64log_cpu.txt & disown
-nohup python dqn_mlp_cartpole.py --batch_size=128 > b128log_cpu.txt & disown
-nohup python dqn_mlp_cartpole.py --batch_size=256 > b256log_cpu.txt & disown
